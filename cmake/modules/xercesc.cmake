5df02617149d9a6dc8d3e418c454a7b691ebf04d
ManuelLerchner <manuel.lerchner1111@gmail.com>
2023-12-18 17:53:50 +0100

    fix cmake to run on rechnerhalle

--- cmake/modules/xercesc.cmake
+++ cmake/modules/xercesc.cmake
@@ -1,18 +1,3 @@
-# Set download timestamp policy
-cmake_policy(SET CMP0135 NEW)
+# Load the XercesC package
 
-# Enable FetchContent CMake module
-include(FetchContent)
-
-# Build xerces and make the cmake targets available
-FetchContent_Declare(
-        xercesc
-        URL
-        # xerces-c-3.2.4.zip
-        # https://xerces.apache.org/xerces-c/download.cgi
-        ${PROJECT_SOURCE_DIR}/libs/libxerces-c-dev_3.2.4-2.zip
-        URL_HASH MD5=8a8fb15d71cacee4b91d6fea5454a240
-)
-
-FetchContent_MakeAvailable(xercesc)
-
+find_package(XercesC 3.2.0 REQUIRED)
